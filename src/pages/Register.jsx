c63f335c1c64fd6b68350757a1411e57ad49b051
SSSerg05 <ssserg05@gmail.com>
2023-10-13 12:24:15 +0300

    add Formik in Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,47 @@
-
+// import { Dispatch } from "react"
+import { Formik, Field, Form } from "formik"
+// import { authReducer } from "redux/authSlice";
 
 export const Register = () => { 
+  // const dispatch = Dispatch();
+
+  const handleSubmit = ({userName:name, userEmail:email, userPassword:password}) => {
+
+    const finalUserData = {
+      name,
+      email,
+      password,
+    }
+
+    console.log(finalUserData);
+    
+  }
 
   return(
     <div>
-      Register
+      <h2>Register Your Account</h2>
+      <Formik 
+        initialValues={{ userName: "", userEmail: "", userPassword: "", }}
+        onSubmit={ async (values) => handleSubmit(values) }
+      >
+        <Form>
+          <Field name="userName" type="text" required minlenght={2} />
+          <Field 
+            name="userEmail" 
+            type="email" 
+            // pattern = "/^[a-zA-Z0-9]+@([a-zA-Z0-9]+\.)+[a-zA-Z0-9]{2,4}$/" 
+            required 
+          />
+          <Field 
+            name="userPassword" 
+            type="password" 
+            minlenght={7}
+            required 
+          />
+          <button type="submit">Submit</button>
+        </Form>
+
+      </Formik>
     </div>
   )
 }
