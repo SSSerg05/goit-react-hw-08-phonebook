888764823273be44c1b70f7ab90aa53c8af0c768
SSSerg05 <ssserg05@gmail.com>
2023-10-14 21:23:29 +0300

    fix forms
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/components/Forms/FormLogin/FormLogin.jsx
+++ src/components/Forms/FormLogin/FormLogin.jsx
@@ -31,15 +31,6 @@ export const FormLogin = () => {
     onSubmit: async (values) => handleSubmit(values),
     validationSchema: Yup.object({
 
-      userName: Yup.string()
-        .min(3, 'Must be at least 3 characters')
-        .max(40, 'Must be less than 40 characters')
-        .matches(
-          /^[\w-/']+$/,
-          'Cannot contain special characters or spaces'
-        )
-        .required('Username is required'),
-
       userEmail: Yup.string()
         .matches(
           /^[\w._%+-]+@[\w.-]+\.[A-Za-z]{2,}$/i,
