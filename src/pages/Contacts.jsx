4af703ecff9f1a847fe1dff2ca93ada207d69204
SSSerg05 <ssserg05@gmail.com>
2023-10-15 09:03:23 +0300

    fix
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,12 @@
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 // import { Navigate } from 'react-router-dom';
 
-import {selectAuthetification, } from 'redux/selectors';
+// import {selectAuthetification, } from 'redux/selectors';
 import { Container } from "components/App/App.styled";
 import { Phonebook } from 'components/Phonebook/Phonebook';
 
 export const Contacts = () => { 
-  
+
   // const authetification = useSelector(selectAuthetification);
 
   return(
