919eb465c5541e1c4b6bfa553440124b45d4b302
SSSerg05 <ssserg05@gmail.com>
2023-10-16 19:11:00 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, lazy, useState } from 'react';
+import { useEffect, lazy, } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from "react-router-dom";
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { createPortal } from "react-dom";
 import { MdClose } from 'react-icons/md';
 import PropTypes from 'prop-types';
@@ -11,31 +11,49 @@ const modalRoot = document.querySelector('#modal-root');
 export const Modal = ({children, src, tags, onClose}) => {
   
   const [loaded, setLoaded ] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+
+// const handleClickOpen = useCallback((e) => {
+//   setOpen(true)
+// }, [])
 
   useEffect(() => {
     window.addEventListener('keydown', handleKeyDown);
     setLoaded(true);
     return window.removeEventListener('keydown', handleKeyDown)
   }, [loaded])
-  
+
+
   // flag loading img
   const onLoadedLargeImage = () => {
     setLoaded(false);
   }
 
   // close modal for press in ESC
-  const handleKeyDown = e => {
+  const handleKeyDown = useCallback(e => {
     if (e.code === 'Escape') {
       onClose();
+      // toggleModal();
     }
+  }, [])
+
+
+
+  // відкриття / закриття модалки
+  const toggleModal = () => {
+    setShowModal( !showModal );
   }
 
   // close modal for click in backdrop || button
   const handleBackdropClick = e => { 
     if (e.currentTarget === e.target) { 
-       onClose();
+      onClose();
+      // toggleModal();
     }
   }
+
+
 
   return createPortal(
     <Overlay onClick={ handleBackdropClick }>
