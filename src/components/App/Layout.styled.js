7b257fc536aad7f88b707665f61e4c74ea69e53b
SSSerg05 <ssserg05@gmail.com>
2023-10-18 13:00:57 +0300

    fix

--- src/components/App/Layout.styled.js
+++ src/components/App/Layout.styled.js
@@ -9,7 +9,7 @@ import { NavLink } from "react-router-dom";
 // `;
 
 export const Container = styled.div`
-  max-width: 960px;
+  /* max-width: 720px; */
   min-width: 270px;
   margin: 0 auto;
   padding: 0 16px;
